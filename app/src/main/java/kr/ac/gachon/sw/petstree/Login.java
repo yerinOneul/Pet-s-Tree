492786299fc7e28b228563c36335e819fde2a367
CyberFramework <mhcho98@gmail.com>
2021-05-03 13:51:15 +0900

    add login, sign in UI

---
+++ app/src/main/java/kr/ac/gachon/sw/petstree/Login.java
@@ -0,0 +1,26 @@
+package kr.ac.gachon.sw.petstree;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signUp = (TextView)findViewById(R.id.sign_up);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/kr/ac/gachon/sw/petstree/SignUp.java
@@ -0,0 +1,20 @@
+package kr.ac.gachon.sw.petstree;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
