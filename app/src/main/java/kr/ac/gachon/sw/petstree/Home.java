879e57a34c8095344ab1bb9b3b32d821297b5ded
CyberFramework <mhcho98@gmail.com>
2021-05-10 10:12:35 +0900

    Add post list layout using recycler view

--- app/src/main/java/kr/ac/gachon/sw/petstree/Home.java
+++ app/src/main/java/kr/ac/gachon/sw/petstree/Home.java
@@ -3,18 +3,40 @@ package kr.ac.gachon.sw.petstree;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class Home extends AppCompatActivity {
+
+    private PostListAdapter postListAdapter = new PostListAdapter();
+    public ArrayList<Post> postArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // 게시글 목록 생성
+        RecyclerView postList = findViewById(R.id.recycler_post_list);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setReverseLayout(true);
+        llm.setStackFromEnd(true); // 먼저 생성된 포스트가 가장 밑으로
+        postList.setLayoutManager(llm);
+        postList.setAdapter(postListAdapter);
+
+        // postArrayList에 포스트 정보 추가 후 게시글 목록에 할당
+        postArrayList.add(new Post("Test Title", "Test Author", "Test Body", 3));
+        postArrayList.add(new Post("Test Title 2", "Test Author 2", "Test Body", 2));
+        postListAdapter.setItems(postArrayList);
+
 
         FloatingActionButton fab = findViewById(R.id.plus_btn);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +46,7 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
     }
 }
---
+++ app/src/main/java/kr/ac/gachon/sw/petstree/PostListAdapter.java
@@ -0,0 +1,59 @@
+package kr.ac.gachon.sw.petstree;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PostListAdapter extends RecyclerView.Adapter<PostListAdapter.ViewHolder> {
+
+    private ArrayList<Post> items = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public PostListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostListAdapter.ViewHolder viewHolder, int position) {
+
+        Post item = items.get(position);
+
+        viewHolder.title.setText(item.getTitle());
+        viewHolder.author.setText(item.getAuthor());
+        viewHolder.num_comments.setText(item.getNum_comments());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void setItems(ArrayList<Post> items) {
+        this.items = items;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title, author, num_comments;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.post_title);
+            author = itemView.findViewById(R.id.post_author);
+            num_comments = itemView.findViewById(R.id.num_comments);
+        }
+    }
+}
