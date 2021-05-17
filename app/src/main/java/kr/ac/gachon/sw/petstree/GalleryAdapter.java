19adfde43ea3e4bc516de16f206bd054078c5cb9
SeonDahye <adad05011@gachon.ac.kr>
2021-05-17 23:52:29 +0900

    image test

--- app/src/main/java/kr/ac/gachon/sw/petstree/GalleryAdapter.java
+++ app/src/main/java/kr/ac/gachon/sw/petstree/GalleryAdapter.java
@@ -1,2 +1,68 @@
-package kr.ac.gachon.sw.petstree;public class GalleryAdapter {
+package kr.ac.gachon.sw.petstree;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+public class GalleryAdapter extends RecyclerView.Adapter <GalleryAdapter.GalleryViewHolder> {
+    private ArrayList<String> mDataset;
+    private Activity activity;
+
+    static class GalleryViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+        GalleryViewHolder(CardView v){
+            super(v);
+            cardView = v;
+        }
+    }
+    public GalleryAdapter(Activity activity, ArrayList<String> myDataset){
+        mDataset = myDataset;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public GalleryAdapter.GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        CardView v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_gallery, parent, false);
+        GalleryViewHolder vh = new GalleryViewHolder(v);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(final GalleryViewHolder holder, int position) {
+        CardView v = holder.cardView;
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("imagePath", mDataset.get(holder.getAdapterPosition()));
+                activity.setResult(Activity.RESULT_OK, resultIntent);
+                activity.finish();
+            }
+        });
+
+        ImageView imageView = holder.cardView.findViewById(R.id.imageView);
+        // override = 이미지 사이즈 조절
+        Glide.with(activity).load(mDataset.get(position)).centerCrop().override(500).into(imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
 }
--- app/src/main/java/kr/ac/gachon/sw/petstree/Write.java
+++ app/src/main/java/kr/ac/gachon/sw/petstree/Write.java
@@ -1,16 +1,24 @@
 package kr.ac.gachon.sw.petstree;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +29,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class Write extends AppCompatActivity {
     private Button save_btn;
     private Button cancel_btn;
+    private ImageButton image_btn;
     private static final String TAG = "Write";
     FirebaseUser user;
 
@@ -30,15 +39,55 @@ public class Write extends AppCompatActivity {
         setContentView(R.layout.activity_writing);
         save_btn = findViewById(R.id.save_btn);
         cancel_btn = findViewById(R.id.cancel_btn);
+        image_btn = findViewById(R.id.image_btn);
+
         save_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addPost();
+            }
+        });
+
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startToast("게시글 작성을 취소했습니다.");
                 Intent intent = new Intent(getApplicationContext(), Home.class);
                 startActivity(intent);
             }
         });
 
+        image_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myStartActivity(GalleryActivity.class, "image");
+            }
+        });
+    }
+
+    // 사진 선택 시 이미지를 view에 띄워준다.
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 0:{
+                if(requestCode == Activity.RESULT_OK){
+                   String imagePath =data.getStringExtra("imagePath");
+
+                    LinearLayout parent = findViewById(R.id.contentsLayout);
+
+                    ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT );
+                    ImageView imageView = new ImageView(Write.this);
+                    imageView.setLayoutParams(layoutParams);
+                    Glide.with(this).load(imagePath).override(1000).into(imageView);
+                    parent.addView(imageView);
+
+                    EditText editText = new EditText(Write.this);
+                    editText.setLayoutParams(layoutParams);
+                }
+                break;
+            }
+        }
     }
 
     private void addPost(){
@@ -49,6 +98,8 @@ public class Write extends AppCompatActivity {
             user = FirebaseAuth.getInstance().getCurrentUser();
             Write_Info write_Info = new Write_Info(title, contents, user.getUid());
             uploader(write_Info);
+            Intent intent = new Intent(getApplicationContext(), Home.class);
+            startActivity(intent);
         }else {
             startToast("내용을 입력해주세요.");
         }
@@ -74,6 +125,12 @@ public class Write extends AppCompatActivity {
 
     private void startToast(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void myStartActivity(Class c, String media){
+        Intent intent = new Intent(this, c);
+        intent.putExtra("media", media);
+        startActivityForResult(intent, 0);
     }
 
 }
