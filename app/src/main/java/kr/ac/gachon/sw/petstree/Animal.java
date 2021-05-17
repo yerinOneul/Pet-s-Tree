27aed1aee20d84e3100b11afe5ab8e6cb3ac8832
Yerin Hong <ka030221@naver.com>
2021-05-18 01:24:26 +0900

    add animal adapter

---
+++ app/src/main/java/kr/ac/gachon/sw/petstree/Animal.java
@@ -0,0 +1,50 @@
+package kr.ac.gachon.sw.petstree;
+
+public class Animal {
+    private String image,age,sex,weight,kind,place,shelter;
+
+    public void setImage(String image){
+        this.image = image;
+    }
+    public String getImage(){
+        return image;
+    }
+    public void setAge(String age){
+        this.age = age;
+    }
+    public String getAge(){
+        return age;
+    }
+    public void setSex(String sex){
+        this.sex = sex;
+    }
+    public String getSex(){
+        return sex;
+    }
+    public void setWeight(String weight){
+        this.weight = weight;
+    }
+    public String getWeight(){
+        return weight;
+    }
+    public void setKind(String kind){
+        this.kind = kind;
+    }
+    public String getKind(){
+        return kind;
+    }
+    public void setPlace(String place){
+        this.place = place;
+    }
+    public String getPlace(){
+        return place;
+    }
+    public void setShelter(String shelter){
+        this.shelter = shelter;
+    }
+    public String getShelter(){
+        return shelter;
+    }
+
+
+}
